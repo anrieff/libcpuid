11e57e78a20fb91abcc56c591cf8708a6cf1c95a
Xorg <xorgbreaker@gmail.com>
2022-09-03 20:48:12 +0200

    Remove Debian package (#165)

--- bump_version.sh
+++ bump_version.sh
@@ -23,9 +23,8 @@ sed -i "s|LIBCPUID_AGE=.*|LIBCPUID_AGE=$(echo $SO_VERSION | cut -d. -f2)|" "$GIT
 sed -i "s|LIBCPUID_REVISION=.*|LIBCPUID_REVISION=$(echo $SO_VERSION | cut -d. -f3)|" "$GIT_DIR/configure.ac"
 sed -i "s|VERSION \"$OLD_VERSION\"|VERSION \"$NEW_VERSION\"|" "$GIT_DIR/CMakeLists.txt"
 sed -i "s|set(SOVERSION .*)|set(SOVERSION $(echo $SO_VERSION | cut -d. -f1))|" "$GIT_DIR/CMakeLists.txt"
-sed -i "1s/^/libcpuid \($NEW_VERSION\) unstable; urgency=low\n/" "$GIT_DIR/debian/changelog"
 sed -i "s|$OLD_VERSION|$NEW_VERSION|g" "$GIT_DIR/libcpuid/"{Makefile.x64,Makefile.x86,libcpuid.dsp,libcpuid_vc10.vcxproj,libcpuid_vc71.vcproj}
 sed -i "s|\\\version  $OLD_VERSION|\\\version  $NEW_VERSION|" "$GIT_DIR/libcpuid/libcpuid.h"
 
 echo "Model from https://github.com/anrieff/libcpuid/commit/a6123e8139ce73e26b5711c3e119b5b2bc9c9378"
-echo -e "Also, make appropriate changes in follow files:\n- ChangeLog\n- debian/changelog\n- configure.ac\n- libcpuid/libcpuid.h"
+echo -e "Also, make appropriate changes in follow files:\n- ChangeLog\n- configure.ac\n- libcpuid/libcpuid.h"
