ba80796d3cead8002f7942f60e99be552a03813d
The Tumultuous Unicorn Of Darkness <the-tumultuous-unicorn-of-darkness@gmx.com>
2023-04-01 17:08:24 +0200

    DB: add Intel Raptor Lake-P
    Related to InstLatx64/InstLatx64@d4a4849a9063deb4fc1d9a7726fefd439da3888d
    Wikipedia page: https://en.wikipedia.org/wiki/Raptor_Lake#Raptor_Lake-P

--- libcpuid/recog_intel.c
+++ libcpuid/recog_intel.c
@@ -461,10 +461,8 @@ const struct match_entry_t cpudb_intel[] = {
 	{  6,  7, -1, -1, 183, -1,    -1,    -1, NC, CORE_|_I_|_3  ,_13xxx, "Raptor Lake-S (Core i3)"    },
 	{  6, 15, -1, -1, 191, -1,    -1,    -1, NC, CORE_|_I_|_5  ,_13xxx, "Raptor Lake-S (Core i5)"    }, // "Golden Cove" cores
 	{  6, 15, -1, -1, 191, -1,    -1,    -1, NC, CORE_|_I_|_3  ,_13xxx, "Raptor Lake-S (Core i3)"    }, // "Golden Cove" cores
-	//{  6, ??, -1, -1, ???, -1,    -1,    -1, NC, CORE_|_I_|_9  ,_13xxx, "Raptor Lake-P (Core i9)"    },
-	//{  6, ??, -1, -1, ???, -1,    -1,    -1, NC, CORE_|_I_|_7  ,_13xxx, "Raptor Lake-P (Core i7)"    },
-	//{  6, ??, -1, -1, ???, -1,    -1,    -1, NC, CORE_|_I_|_5  ,_13xxx, "Raptor Lake-P (Core i5)"    },
-	//{  6, ??, -1, -1, ???, -1,    -1,    -1, NC, CORE_|_I_|_3  ,_13xxx, "Raptor Lake-P (Core i3)"    },
+	{  6, 10,  2, -1, 186, -1,    -1,    -1, NC, CORE_|_I_|_7  ,_13xxx, "Raptor Lake-P (Core i7)"    },
+	{  6, 10,  2, -1, 186, -1,    -1,    -1, NC, CORE_|_I_|_5  ,_13xxx, "Raptor Lake-P (Core i5)"    },
 	/* F   M   S  EF   EM   C     L2     L3               Brand */
 
 
